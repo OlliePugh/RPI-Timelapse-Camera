855849a92d8faee8c2935f6fccad7b8134dd8624
Ollie Pugh <oliver.pugh@mac.com>
2020-04-25 19:59:58 +0100

    Removed threading as it was not required

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import sys
-import mysql.connector
-from mysql.connector import errorcode
 from picamera import PiCamera
 import time
 import datetime as dt
-from db_credentials import config as db_config
-import threading
 
 class TimelapseCamera:
     directory = "not-in-db"
-    cameraWarmUpTime = 5
+    cameraWarmUpTime = 2
 
     def __init__(self, interval):
         self.interval=interval # how many minutes between each photo
         self.camera = PiCamera()
         self.camera.resolution = (1024, 768)
 
-        self.thread = threading.Thread(target = self.thread_routine)
-        self.thread.daemon = True
-        self.thread.start()
-
-    def thread_routine(self):
-        while True:
-            self.take_photo()
-            time.sleep(self.interval*60-TimelapseCamera.cameraWarmUpTime)
-
-
     def take_photo(self):
         print("Taking photo")
         self.camera.start_preview()
         time.sleep(TimelapseCamera.cameraWarmUpTime)
-        self.camera.capture(TimelapseCamera.directory + "/" + str(time.time())+".jpg")
+        filename = TimelapseCamera.directory+"/"+str(int(time.time()))+".jpg"
+        self.camera.capture(filename)
         self.camera.stop_preview()
-        print("Finished taking photo with name: " + str(time.time()) + ".jpg")
+        print("Finished taking photo with name: " + filename)
 
 def main():
-    tlCamera = TimelapseCamera(1)  #make static list of all threads that can be closed
+    tlCamera = TimelapseCamera(60)  #make static list of all threads that can be closed
+    while True:
+        tlCamera.take_photo()
+        time.sleep(tlCamera.interval-TimelapseCamera.cameraWarmUpTime)
 
 if __name__ == "__main__":
     main()
-    while True:
-        pass
